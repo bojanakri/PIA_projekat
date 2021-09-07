4faf99cb64a8aa2c0b9cf4f3deeff4a6d7994f30
Bojana Krivokapic <48254026+bojanakri@users.noreply.github.com>
2021-09-07 16:15:52 +0200

    Create delegat.model.ts

---
+++ frontend/src/app/model/delegat.model.ts
@@ -0,0 +1,12 @@
+export class Delegat{
+    korime: string;
+    email: string;
+    lozinka: string;
+    ime: string;
+    prezime: string;
+    tip: string;
+    zemlja: string;
+    odobren: boolean;
+    brojTakmicenja: number;
+    dodat:boolean;
+}
